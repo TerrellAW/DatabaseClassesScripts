d771bf695852cfe47cd5c140f5807aed6bc89b00
Bones <terraw1999@proton.me>
2026-03-29 12:35:08 -0600

    "Add: Insert statements (WIP)"

--- 2/scripts/accounting.sql
+++ 2/scripts/accounting.sql
@@ -1,10 +1,47 @@
 DECLARE
 
-	
+	-- Cursors
+	CURSOR c_new_trans IS
+		SELECT *
+		FROM new_transactions;
 
 BEGIN
 
-	
+	-- Loop through new transactions in new_transactions table
+	FOR r_new_trans IN c_new_trans LOOP
+
+		-- TODO: Account foreign key needs to be in table
+		-- TODO: Update account balance with accounting formula
+
+		-- Insert into transaction detail
+		INSERT INTO transaction_detail (
+			account_no,
+			transaction_no,
+			transaction_type,
+			transaction_amount
+		)
+		VALUES (
+			r_new_trans.account_no,
+			r_new_trans.transaction_no,
+			r_new_trans.transaction_type,
+			r_new_trans.transaction_amount
+		);
+
+		-- Insert into transaction_history
+		INSERT INTO transaction_history (
+			transaction_no,
+			transaction_date,
+			description
+		)
+		VALUES (
+			r_new_trans.transaction_no,
+			r_new_trans.transaction_date,
+			r_new_trans.description
+		);
+
+		-- TODO: Exception handling
+
+	END LOOP;
 
 END;
 /
